bdb82f3b26ce2573fcad88a657c97706501d503d
Ben Brammer (s5101936) <s5101936@mac42948.student.bournemouth.ac.uk>
2019-04-03 11:30:26 +0100

    Initial Commit

---
+++ gitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitDemo
+//
+//  Created by Ben Brammer (s5101936) on 4/3/19.
+//  Copyright © 2019 Ben Brammer (s5101936). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
